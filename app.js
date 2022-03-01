373bf523ee1cce7f8f0cbe6c615c7d17494062d3
Saake <youaretube719@gmail.com>
2022-03-01 06:54:46 +0530

    bare minimum working RPS

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -19,12 +21,11 @@ function generateComputerChoice() {
         computerChoice = 'rock'
     }
     if (randomNumber === 2) {
-        computerChoice = 'paper'
-    }
-    if (randomNumber === 3) {
         computerChoice = 'scissors'
     }
-
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
     computerChoiceDisplay.innerHTML = computerChoice
 
 }
@@ -51,5 +52,5 @@ function getResult() {
     if (computerChoice === 'paper' && userChoice === 'scissors') {
         result = `you win!`
     }
-
+    resultDisplay.innerHTML = result
 }
